ef751a0b217573675c7a17a0c340d785e48efad3
blank <yongchun.long@pitayagames.com>
2019-01-03 15:18:02 +0800

    提交第一版程序

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rancher-test
+
---
+++ service.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello web!!!")
+}
+
+func main() {
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":8088", nil)
+	if err != nil {
+		fmt.Println("listen and server: ", err)
+	}
+}
